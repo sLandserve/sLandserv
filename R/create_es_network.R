32cb37416fed0959ec12ca4f931decc8e1086858
laurajanegraham <laurajaneegraham@gmail.com>
2018-08-13 19:33:44 +0100

    updated network metric calculation

--- R/create_es_network.R
+++ R/create_es_network.R
@@ -66,12 +66,14 @@ create_es_network <- function(ls_supply,
     return(list(network = NA, params = params))
   }
 
-  ee_network <-  igraph::graph_from_adjacency_matrix(as.matrix(net_links), diag = FALSE)
-  params$ee_density <- igraph::edge_density(ee_network)
-  params$ee_centr_betw <- igraph::centr_betw(ee_network)$centralization
-  params$ee_centr_degree <- igraph::centr_degree(ee_network, mode = "in")$centralization # got in here because no directionality in network - rethink if we have directionality
-  params$ee_edge_per_node_mean <- mean(igraph::degree(ee_network, mode = "in"))
-  params$ee_edge_per_node_sd <- sd(igraph::degree(ee_network, mode = "in"))
+  # calculate some network metrics
+  es_network <-  igraph::graph_from_adjacency_matrix(as.matrix(net_links), diag = FALSE)
+  es_network <- igraph::as.undirected(es_network)
+  params$es_density <- igraph::edge_density(es_network)
+  params$es_centr_betw <- igraph::centr_betw(es_network)$centralization
+  params$es_centr_degree <- igraph::centr_degree(es_network, mode = "in")$centralization # got in here because no directionality in network - rethink if we have directionality
+  params$es_edge_per_node_mean <- mean(igraph::degree(es_network, mode = "in"))
+  params$es_edge_per_node_sd <- sd(igraph::degree(es_network, mode = "in"))
 
   # get network in correct format
   network <- net_links %>% tibble::as_tibble() %>%
